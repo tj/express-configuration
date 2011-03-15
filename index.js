6c0788ea14c44751d03d95d1ae34b74a168a767a
Guillermo Rauch <rauchg@gmail.com>
2011-03-14 18:59:17 -0700

    SSL support

--- index.js
+++ index.js
@@ -10,80 +10,83 @@
  */
 
 var express = require('express')
-  , Server = express.HTTPServer
-    ? express.HTTPServer
-    : express.Server;
+  , servers = express.Server 
+            ? [express.Server]
+            : [express.HTTPServer, express.HTTPSServer];
 
-// Proxy listen
+servers.forEach(function (Server) {
 
-var listen = Server.prototype.listen;
+  // Proxy listen
 
-/**
- * Proxy listen() to provide async support.
- *
- * @api public
- */
+  var listen = Server.prototype.listen;
 
-Server.prototype.listen = function(){
-  if (this.__config) {
-    this.__listen = arguments;
-  } else {
-    listen.apply(this, arguments);
-  }
-};
+  /**
+   * Proxy listen() to provide async support.
+   *
+   * @api public
+   */
 
-// Proxy listenFD
-
-var listenFD = Server.prototype.listenFD;
-
-/**
- * Proxy listen() to provide async support.
- *
- * @api public
- */
-
-Server.prototype.listenFD = function(){
-  if (this.__config) {
-    this.__listenFD = arguments;
-  } else {
-    listenFD.apply(this, arguments);
-  }
-};
-
-// Proxy configure
-
-var configure = Server.prototype.configure;
-
-/**
- * Proxy configure() to provide async support.
- *
- * @api public
- */
-
-Server.prototype.configure = function(env, fn){
-  var self = this;
-  this.__config = this.__config || 0
-
-  if (typeof env === 'function') {
-    fn = env, env = 'all';
-  }
-
-  if ('all' == env || env == this.settings.env) {
-    if (fn.length) {
-      ++this.__config;
-      fn.call(this, function(err){
-        if (err) throw err;
-        if (!--self.__config) {
-          if (self.__listen) {
-            listen.apply(self, self.__listen);
-          } else if (self.__listenFD) {
-            listenFD.apply(self, self.__listenFD);
-          }
-        }
-      });
+  Server.prototype.listen = function(){
+    if (this.__config) {
+      this.__listen = arguments;
     } else {
-      fn.call(this);
+      listen.apply(this, arguments);
     }
-  }
-  return this;
-};
+  };
+
+  // Proxy listenFD
+
+  var listenFD = Server.prototype.listenFD;
+
+  /**
+   * Proxy listen() to provide async support.
+   *
+   * @api public
+   */
+
+  Server.prototype.listenFD = function(){
+    if (this.__config) {
+      this.__listenFD = arguments;
+    } else {
+      listenFD.apply(this, arguments);
+    }
+  };
+
+  // Proxy configure
+
+  var configure = Server.prototype.configure;
+
+  /**
+   * Proxy configure() to provide async support.
+   *
+   * @api public
+   */
+
+  Server.prototype.configure = function(env, fn){
+    var self = this;
+    this.__config = this.__config || 0
+
+    if (typeof env === 'function') {
+      fn = env, env = 'all';
+    }
+
+    if ('all' == env || env == this.settings.env) {
+      if (fn.length) {
+        ++this.__config;
+        fn.call(this, function(err){
+          if (err) throw err;
+          if (!--self.__config) {
+            if (self.__listen) {
+              listen.apply(self, self.__listen);
+            } else if (self.__listenFD) {
+              listenFD.apply(self, self.__listenFD);
+            }
+          }
+        });
+      } else {
+        fn.call(this);
+      }
+    }
+    return this;
+  };
+});
